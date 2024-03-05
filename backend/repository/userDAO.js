298baa1003d1219f0b3c92291c5d004733e7a554
John Quinn <johnjquinn01@outlook.com>
2024-03-05 10:32:01 -0500

    added reqs to user router and user dao

---
+++ backend/repository/userDAO.js
@@ -0,0 +1,2 @@
+const {DynamoDBClient} = require('@aws-sdk/client-dynamodb');
+const {DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand, DeleteCommand} = require("@aws-sdk/lib-dynamodb");
