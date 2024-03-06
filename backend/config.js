68037252e92b501b369aa7dd1f3e8c462bab757d
Sagar Prasad <saprasad@csumb.edu>
2024-03-06 07:34:04 -0800

    worked on adding jwt token and middleware

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-key";
+const PORT = +process.env.PORT || 3001;
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+module.exports = { SECRET_KEY, PORT, BCRYPT_WORK_FACTOR };
---
+++ backend/util/tokens.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config.js");
+
+function createToken(user) {
+    let payload = {
+        id: user.employee_id,
+        username: user.username,
+        role: user.role
+    }
+
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: "60m" });
+}
+
+module.exports = { createToken } 
