a944cb9fdd1b15ec9816f7afe12eea716e2e8182
John Quinn <johnjquinn01@outlook.com>
2024-03-06 16:21:05 -0500

    added register

--- backend/service/userService.js
+++ backend/service/userService.js
@@ -3,115 +3,82 @@ const uuid = require('uuid');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
+const { BCRYPT_WORK_FACTOR } = require('../config');
+const jsonschema = require('jsonschema');
+const userRegisterSchema = require('../schemas/userRegisterSchema.json');
 dotenv.config();
 
-const saltRounds = 10;
-const secretKey = process.env.PASS_SECRET_KEY;
 
 const getAllUsers = async () => {
     const users = await userDAO.getAllUsers();
+    if(!users) return {response: false, errors: "No users"};
+    return {response: true, message: "Got all users", users};
 };
 
-const getUser = async id => {
-    if(!id) return null;
-    const user = await userDAO.getUserById(id);
-    return user;
+const getUser = async user_id => {
+    if(!user_id) return {response: false, errors: "No user id provided"}
+    const user = await userDAO.getUserById(user_id);
+    if(!user) return {response: false, errors: `No user found with id ${user_id}`}
+    return {response: true, message: `Got user with id ${user_id}`, user};
 };
 
 const getUserByUsername = async username => {
-    if(!username) return null;
+    if(!username) return {response: false, errors: "No username provided"};
     const user = await userDAO.getUserByUsername(username);
-    return user;
+    if(!user) return {response: false, errors: `No user found with username ${username}`};
+    return {response: true, message: `Got user with username ${username}`, user};
 };
 
-const registerUser = async (username, password, email) => {
-
-    if(!username || !password || !email) return null;
-    const user_id = uuid.v4();
-    const enc_password = await bcrypt.hash(password, saltRounds);
-    let user = {
-        user_id,
-        username,
-        password: enc_password,
-        email
-    };
-    const data = await userDAO.postUser(user);
-    return data;
-};
-
-const loginUser = async (username, password) => {
-    if(!username || !password) return null;
-    const foundUser = await getUserByUsername(username);
-    if(!foundUser || !(await bcrypt.compare(password, foundUser.password))) return null;
-    const token = jwt.sign(
-        {
-            user_id: foundUser.user_id,
-            username: foundUser.username,
-            email: foundUser.email
-        },
-        secretKey,
-        {
-            expiresIn: "15m"
-        }
-    );
-    return token;
-};
-
-const authenticateUser = token => {
-    if(!token) return null;
-    return jwt.verify(token, secretKey); 
-};
 
 const deleteUser = async user_id => {
-    if(!user_id) return null;
+    if(!user_id) return {repsonse: false, errors: "No user id provided"};
     const data = await userDAO.deleteUser(user_id);
-    return data;
+    if(!data) return {response: false, errors: `Could not delete user with id ${user_id}`};
+    return {response: true, message: `Deleted user with id ${user_id}`};
 };
 
-const validateUser = (username, password) => {
-    
+const registerUser = async (receivedData) => {
+    const validated = validateRegister(receivedData);
+    console.log(receivedData);
+    if(!validated.response) return {response: false, errors: validated.errors};
+    const foundUser = await userDAO.getUserByUsername(receivedData.username);
+    if(foundUser) return {response: false, errors: `User already exists with username ${receivedData.username}`};
+    const user_id = uuid.v4();
+    const teams = [];
+    const my_pokemons = [];
+    const enc_password = await bcrypt.hash(receivedData.password, BCRYPT_WORK_FACTOR);
+    const newUser = {
+        user_id,
+        username: receivedData.username,
+        password: enc_password,
+        name: receivedData.name,
+        email: receivedData.email,
+        role: receivedData.role ? receivedData.role : "user",
+        teams,
+        my_pokemons
+    };
+    const data = await userDAO.postUser(newUser);
+    if(!data) return {response: false, errors: "Could not create user"};
+    return {response: true, message: `Successfully created user ${receivedData.username}`};
 }
 
-// const registerUser = async (receivedData) => {
-//     let employee_id = uuid.v4();
-//     let teams = []
+const validateRegister = receivedData => {
+    const validator = jsonschema.validate(receivedData, userRegisterSchema);
+    if(!validator.valid){
+        const errs = validator.errors.map(e => e.stack);
+        return {response: false, errors: errs};
+    }
+    return {response: true};
+};
 
-//     let { response, errors } = validateRegister(receivedData);
-//     if (!response) return { response: false, errors: errors }
+const loginUser = async receivedData => {
 
-//     let duplicatedUser = await getUserByUsername(receivedData.username);
-//     if (duplicatedUser) return { response: false, errors: "Duplicated username" }
-
-//     let hashedPassword = await bcrypt.hash(receivedData.password, BCRYPT_WORK_FACTOR);
-
-//     let data = await userDAO.createUser({
-//         employee_id: employee_id,
-//         username: receivedData.username,
-//         password: hashedPassword,
-//         email: receivedData.email,
-//         teams: teams
-//     });
-//     return { response: true, message: "user created" };
-
-// }
-
-// function validateRegister(receivedData) {
-//     const validator = jsonschema.validate(receivedData, userRegisterSchema);
-//     if (!validator.valid) {
-//         const errs = validator.errors.map(e => e.stack);
-//         logger.error(errs);
-//         //throw new BadRequestError(errs); 
-//         //return { response: false, errors: errs };
-//     }
-//     return { response: true };
-// }
+};
 
 module.exports = {
     getAllUsers,
     getUser,
     getUserByUsername,
     registerUser,
-    loginUser,
-    authenticateUser,
     deleteUser
 };
