e8fa3ad1518b8734b7b149bf344c8ef2a11e6572
Sagar Prasad <sagarprasad63574@gmail.com>
2024-04-08 19:37:20 -0700

    worked on the aws sam for registering and logging user

---
+++ SocialMediaPokemonBackend/users/login-function/login-handler.mjs
@@ -0,0 +1,111 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import jsonschema from 'jsonschema';
+import bcrypt from 'bcryptjs';
+
+const client = new DynamoDBClient({ region: 'us-west-1' });
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event) => {
+    if (!event.body) {
+        return { statusCode: 400, body: "No Body" };
+    }
+    const receivedData = JSON.parse(event.body);
+    const validated = validateLogin(receivedData);
+    if (!validated.response) return {
+        statusCode: 400,
+        body: JSON.stringify({
+            response: false,
+            errors: validated.errors,
+        }),
+    };
+
+    const foundUser = await getUserByUsername(receivedData.username);
+    if (!foundUser) return {
+        statusCode: 400,
+        body: JSON.stringify({
+            response: false,
+            errors: "User does not exist",
+        }),
+    };
+
+    if (!(await bcrypt.compare(receivedData.password, foundUser.password))) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                response: false,
+                errors: "Incorrect password",
+            }),
+        };
+    };
+
+
+    const user = {
+        user_id: foundUser.user_id,
+        username: foundUser.username,
+        name: foundUser.name,
+        role: foundUser.role
+    };
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            response: true,
+            message: `User ${foundUser.username} logged in successfully`,
+            ...user
+        }),
+    };
+};
+
+const validateLogin = receivedData => {
+    const validator = jsonschema.validate(receivedData, userLoginSchema);
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack.substring(9));
+        return { response: false, errors: errs };
+    }
+    return { response: true };
+};
+
+const getUserByUsername = async (username) => {
+    const command = new QueryCommand({
+        TableName: process.env.TABLE_NAME,
+        IndexName: "username-index",
+        KeyConditionExpression: "#u = :u",
+        ExpressionAttributeNames: {
+            "#u": "username"
+        },
+        ExpressionAttributeValues: {
+            ":u": username
+        }
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data.Items[0];
+    } catch (error) {
+        return null;
+    }
+};
+
+const userLoginSchema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "http://example.com/example.json",
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string",
+            "minLength": 5,
+            "maxLength": 30
+        },
+        "password": {
+            "type": "string",
+            "minLength": 5,
+            "maxLength": 20
+        }
+    },
+    "additionalProperties": false,
+    "required": [
+        "username",
+        "password"
+    ]
+};
---
+++ SocialMediaPokemonBackend/users/register-function/register-handler.mjs
@@ -0,0 +1,162 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import jsonschema from 'jsonschema';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcryptjs';
+
+const client = new DynamoDBClient({ region: 'us-west-1' });
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event) => {
+    if (!event.body) {
+        return { statusCode: 400, body: "No Body" };
+    }
+    const receivedData = JSON.parse(event.body);
+    const validated = validateRegister(receivedData);
+    if (!validated.response) return {
+        statusCode: 400,
+        body: JSON.stringify({
+            response: false, 
+            errors: validated.errors,
+        }),
+    };
+
+    const foundUser = await getUserByUsername(receivedData.username);
+    if (foundUser) return {
+        statusCode: 400,
+        body: JSON.stringify({
+            response: false,
+            errors: `User already exists with username ${receivedData.username}`,
+        }),
+    };
+
+    const user_id = uuidv4();
+    const teams = [];
+    const my_pokemons = [];
+    const comments = [];
+    const biography = "";
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(receivedData.password, salt);
+
+    const newUser = {
+        user_id,
+        username: receivedData.username,
+        password: hashedPassword,
+        name: receivedData.name,
+        email: receivedData.email,
+        role: receivedData.role ? receivedData.role : "user",
+        teams,
+        my_pokemons,
+        biography,
+        comments
+    };
+
+    try {
+        let data = await postUser(newUser);
+        if(!data) return {
+            statusCode: 500,
+            body: JSON.stringify({
+                response: false,
+                errors: "Could not create user",
+            }),
+        };
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({
+                response: true,
+                message: `Successfully created user ${receivedData.username}`,
+            }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: error,
+            }),
+        };
+    }
+};
+
+const validateRegister = receivedData => {
+    const validator = jsonschema.validate(receivedData, userRegisterSchema);
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack.substring(9));
+        return { response: false, errors: errs };
+    }
+    return { response: true };
+};
+
+const getUserByUsername = async (username) => {
+    const command = new QueryCommand({
+        TableName: process.env.TABLE_NAME,
+        IndexName: "username-index",
+        KeyConditionExpression: "#u = :u",
+        ExpressionAttributeNames: {
+            "#u": "username"
+        },
+        ExpressionAttributeValues: {
+            ":u": username
+        }
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data.Items[0];
+    } catch (error) {
+        return null;
+    }
+};
+
+
+const postUser = async (user) => {
+    const command = new PutCommand({
+        TableName: process.env.TABLE_NAME,
+        Item: user
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data;
+    } catch (error) {
+        return null;
+    }
+};
+
+const userRegisterSchema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "http://example.com/example.json",
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string",
+            "minLength": 5,
+            "maxLength": 30
+        },
+        "password": {
+            "type": "string",
+            "minLength": 5,
+            "maxLength": 20
+        },
+        "name": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 30
+        },
+        "email": {
+            "type": "string",
+            "minLength": 5,
+            "maxLength": 30,
+            "format": "email"
+        },
+        "role": {
+            "role": "string"
+        }
+    },
+    "additionalProperties": false,
+    "required": [
+        "username",
+        "password",
+        "name",
+        "email"
+    ]
+};
